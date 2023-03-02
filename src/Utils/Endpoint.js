4d70a4408f6fd8a845e5af545fb4f4a3aa1e1fd0
Uzair Ali <40020872+uzairalimurtaza@users.noreply.github.com>
2023-03-02 10:24:54 +0500

    Update Endpoint.js

--- src/Utils/Endpoint.js
+++ src/Utils/Endpoint.js
@@ -1,12 +1,1 @@
-// var Endpoint = "http://localhost:3001";
-// export const Host_Name = "http://localhost:3000";
-// export const WebUrl = "http://localhost:3000";
 
-export const WebUrl = "https://banglabazar.com";
-var Endpoint = "https://api.banglabazar.com";
-export const Host_Name = "https://banglabazar.com";
-
-export default Endpoint;
-// "http://18.220.235.3:3001"
-// "https://api.banglabazar.com"
-//http://192.168.2.119:4000
